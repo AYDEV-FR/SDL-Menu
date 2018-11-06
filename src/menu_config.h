4960c061cf9f14ca677073900d69a8ca45c45460
DES69U-FR <aymeric.deliencourt@isen.yncrea.fr>
2018-11-06 10:53:00 +0100

    Initial Commit

--- src/menu_config.h
+++ src/menu_config.h
@@ -4,8 +4,8 @@ void settings(SDL_Surface *screen);
 void menu_main(SDL_Surface *screen){
   Button menu[2] = {
 
-    initButton(200, 100, "Yolo", "img/settings.png", settings),
-    initButton(400, 300, "Yolo", "img/exit.png", exit),
+    initButton(200, 100, "Yolo", "../img/settings.png", settings),
+    initButton(400, 300, "Yolo", "../img/exit.png", exit),
 
   };
   displayMenu(menu, screen, 2);
@@ -15,8 +15,8 @@ void menu_main(SDL_Surface *screen){
 void settings(SDL_Surface *screen){
   Button menu[2] = {
 
-    initButton(0, 0, "Yolo", "img/back.png", menu_main),
-    initButton(400, 300, "Yolo", "img/exit.png", exit),
+    initButton(0, 0, "Yolo", "../img/back.png", menu_main),
+    initButton(400, 300, "Yolo", "../img/exit.png", exit),
 
   };
 
